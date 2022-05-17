61683cc7613c8d6ee7a7b91ba194bc3d3aa24d2b
bronzo97 <danilobronzo97@hotmail.it>
2022-05-17 15:48:43 +0200

    Exercise done

---
+++ js/app.js
@@ -0,0 +1,9 @@
+alert(`Benvenuto/a nel suggeritore di password: ti porremo un qualche domanda per aiutarci a creare una password per te!`);
+const name = prompt("Quale è il tuo nome?");
+const surname = prompt("Quale è il tuo Cognome");
+const color = prompt("Qual'è il tuo colore preferito?");
+alert(`Ecco come promesso una password per te!`);
+
+
+
+document.getElementById("passwordSuggest").innerHTML = `${name}${surname}${color}`;
